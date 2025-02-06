3a2b3f6a3eed52a8a83f91d8b73c1965c69e1290
이종남 <jongnammac@MacBookAir.local>
2025-02-06 17:00:57 +0900

    feature/exception-api commit

---
+++ src/main/java/com/bo/exception/RemoveException.java
@@ -0,0 +1,10 @@
+package com.bo.exception;
+
+public class RemoveException extends Exception {
+	public RemoveException() {
+		super();
+	}
+	public RemoveException(String message) {
+		super(message); //예외의 상세메시지
+	}
+}
